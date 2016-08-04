8d111e1117a80d346f48346d8a446348f2460df2
Jason Best <skeeterbug@gmail.com>
2016-08-04 14:21:56 -0700

    Cleanup

--- src/SDataService.js
+++ src/SDataService.js
@@ -350,7 +350,6 @@
             };
 
             var builtRequest = request.build();
-            var builtRequestExcludeQuery = request.build(true);
             if (options.httpMethodOverride || (typeof builtRequest === 'string' && builtRequest.length > this.maxGetUriLength))
             {
                 // todo: temporary fix for SalesLogix Dynamic Adapter only supporting json selector in format parameter
@@ -360,7 +359,7 @@
                 ajax.headers['X-HTTP-Method-Override'] = 'GET';
                 ajax.method = 'POST';
                 ajax.body = builtRequest;
-                ajax.url = builtRequestExcludeQuery; // exclude query
+                ajax.url = request.build(true); // exclude query
             }
 
             return this.executeRequest(request, options, ajax);
