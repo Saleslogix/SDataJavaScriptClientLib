73e415e09e97079e664c7184f68aa6830ba92577
Jason Best <jason.best@infor.com>
2017-04-06 09:06:12 -0400

    INFORCRM-15331: Build.

--- deploy/sdata-client-debug.js
+++ deploy/sdata-client-debug.js
@@ -1197,8 +1197,13 @@
 
             // Support for the new compact mode in Saleslogix 8.1 and higher
             if (isDefined(options.compact)) this.uri.setCompact(options.compact);
+
+            // Username/password use options first, constructor params will take priority
             if (isDefined(options.userName)) this.userName = options.userName;
             if (isDefined(options.password)) this.password = options.password;
+            if (isDefined(userName)) this.userName = userName;
+            if (isDefined(password)) this.password = password;
+
             if (isDefined(options.json)) this.json = options.json;
             if (isDefined(options.maxGetUriLength)) this.maxGetUriLength = options.maxGetUriLength;
             if (isDefined(options.timeout)) this.timeout = options.timeout;
@@ -1219,7 +1224,7 @@
                 this.uri = new Sage.SData.Client.SDataUri(options.uri);
             } else {
                 this.uri = new Sage.SData.Client.SDataUri();
-                var parsed = options;
+                var parsed = this.parseUri(options);
                 // Parsed URI
                 if (isDefined(parsed.serverName)) this.uri.setHost(parsed.serverName);
                 if (isDefined(parsed.virtualDirectory)) this.uri.setServer(parsed.virtualDirectory);
@@ -1238,7 +1243,7 @@
                     ? options['url']
                     : options;
 
-            var parsed = (typeof parseUri === 'function') && window.parseUri(url);
+            var parsed = (typeof window.parseUri === 'function') && window.parseUri(url);
             if (parsed)
             {
                 if (parsed.host) result.serverName = parsed.host;
@@ -1254,6 +1259,8 @@
                 if (parsed.port) result.port = parseInt(parsed.port);
                 if (parsed.protocol) result.protocol = parsed.protocol;
             }
+
+            if (typeof options === 'object') S.apply(result, options);
 
             return result;
         },
